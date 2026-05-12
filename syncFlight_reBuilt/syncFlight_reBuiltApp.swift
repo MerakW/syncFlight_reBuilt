5d092a86e77dfdf62e671cb894ce9f813bcac0ab
米拉克 <merak@merakt.cn>
2026-05-12 09:03:00 +0800

    Initial Commit

---
+++ syncFlight_reBuilt/syncFlight_reBuiltApp.swift
@@ -0,0 +1,17 @@
+//
+//  syncFlight_reBuiltApp.swift
+//  syncFlight_reBuilt
+//
+//  Created by Merak Weng on 12/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct syncFlight_reBuiltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ syncFlight_reBuiltTests/syncFlight_reBuiltTests.swift
@@ -0,0 +1,19 @@
+//
+//  syncFlight_reBuiltTests.swift
+//  syncFlight_reBuiltTests
+//
+//  Created by Merak Weng on 12/5/2026.
+//
+
+import Testing
+@testable import syncFlight_reBuilt
+
+struct syncFlight_reBuiltTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
